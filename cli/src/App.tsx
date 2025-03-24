7201fd60b4331ebf3325f021256374e1c5e6d1e1
t0lIk1 <anatolijkuzir@gmail.com>
2025-03-24 12:00:04 +0300

    fix

--- cli/src/App.tsx
+++ cli/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import UserTable from './components/UserTable';
 import Login from './components/Login';
 import Register from './components/Register';
