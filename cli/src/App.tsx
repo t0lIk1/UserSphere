62f40dec5b7325cd6d904e8c15f5c3586c272444
t0lIk1 <anatolijkuzir@gmail.com>
2025-03-24 11:58:41 +0300

    fix: add env cors

--- cli/src/App.tsx
+++ cli/src/App.tsx
@@ -1,39 +1,24 @@
-import React, {useEffect} from 'react';
-import {BrowserRouter as Router, Navigate, Route, Routes, useNavigate} from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import UserTable from './components/UserTable';
 import Login from './components/Login';
 import Register from './components/Register';
 import ProtectedRoute from './components/ProtectedRoute';
-import NotFound from "./components/NotFound.tsx";
+import NotFound from './components/NotFound';
 
 const App: React.FC = () => {
     return (
-        <Router>
-            <AppContent/>
+        <Router basename="sphere/">
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route element={<ProtectedRoute />}>
+                    <Route path="/users" element={<UserTable />} />
+                </Route>
+                <Route path="/" element={<Login />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
         </Router>
-    );
-};
-
-const AppContent: React.FC = () => {
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            navigate('/users');
-        }
-    }, [navigate]);
-
-    return (
-        <Routes>
-            <Route path="/login" element={<Login/>}/>
-            <Route path="/register" element={<Register/>}/>
-            <Route element={<ProtectedRoute/>}>
-                <Route path="/users" element={<UserTable/>}/>
-            </Route>
-            <Route path="/" element={<Navigate to="/login"/>}/>
-            <Route path="*" element={<NotFound />} />
-        </Routes>
     );
 };
 
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as process from "node:process";
 
 async function start() {
   const PORT = process.env.PORT ?? 3000;
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_ORIGIN,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
